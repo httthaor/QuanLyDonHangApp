049d9b0524ddf28b2a17d40d8cfff2fc1b41344b
httthaor <huynhtranthuthao2005@gmail.com>
2025-12-02 13:20:16 +0700

    Cap nhat

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
 
+// Hàm để tự chọn Base URL
+const getBaseUrl = () => {
+  const currentUrl = window.location.href; // Lấy địa chỉ hiện tại trên thanh browser
+  
+  // 1. Nếu đang chạy Localhost
+  if (currentUrl.includes('localhost')) {
+    return 'http://localhost:8080/api';
+  }
+
+  // 2. Nếu đang chạy trên VS Code Port Forwarding 
+  if (currentUrl.includes('5173')) {
+    return window.location.origin.replace('5173', '8080') + '/api';
+  }
+};
+
 const api = axios.create({
-  baseURL: 'https://c58x3567-8080.asse.devtunnels.ms/api',
+  baseURL: getBaseUrl(),
   headers: {
     'Content-Type': 'application/json',
   },
